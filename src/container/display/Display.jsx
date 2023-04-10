55f76279237c0fc00343ebe031ec185c70e7f029
PhamVietHao <89537529+PhamVietHao@users.noreply.github.com>
2023-04-10 13:20:08 +0700

    update

--- src/container/display/Display.jsx
+++ src/container/display/Display.jsx
@@ -1,29 +1,41 @@
 import React from 'react'
 import './display.css'
 import building1 from '../../components/images/building1.png';
-import modal1 from '../../components/images/modal1.png';
+import building2 from '../../components/images/building2.png';
 
+import modal1 from '../../components/images/modal1.png';
+import modal2 from '../../components/images/modal2.png';
 function Display() {
   return (
-    <div className='rise__display section__padding'>
+    <div className='rise__display'>
       <div className='rise__display_buildings'>
           <div className='rise__display_building'>
+            <div className='rise_display_building_img'>
             <img src={building1}></img>
-              <div className='rise__display_modal1'>
-                <img src={modal1}></img>
-              </div>
-          </div>
-      </div>
-      
-      <div className='rise__display_buildings'>
-          <div className='rise__display_building'>
-            <img src={building1}></img>
+            </div>
               <div className='rise__display_modal1'>
                 <img src={modal1}></img>
               </div>
               <div className='rise__display_buildinginfo'>
                 <h3>Beanland Building</h3>
                 <h4>Building 1</h4>
+                <button type="button" id='seebtn'>See more</button>
+              </div>
+          </div>
+      </div>
+      
+      <div className='rise__display_buildings'>
+          <div className='rise__display_building'>
+            <div className='rise_display_building_img'>
+            <img src={building2}></img>
+            </div>
+              <div className='rise__display_modal1'>
+                <img src={modal2}></img>
+              </div>
+              <div className='rise__display_buildinginfo'>
+                <h3>Beanland Building</h3>
+                <h4>Building 1</h4>
+                <button type="button" id='seebtn'>See more</button> 
               </div>
           </div>
       </div>  
--- src/container/display2/Display2.jsx
+++ src/container/display2/Display2.jsx
@@ -1,25 +1,42 @@
 import React from 'react'
 // import './display2.css'
-import building1 from '../../components/images/building1.png';
-import modal1 from '../../components/images/modal1.png';
+import building89 from '../../components/images/building89.png';
+import building10 from '../../components/images/building10.png';
+
+import modal3 from '../../components/images/modal3.png';
+import modal4 from '../../components/images/modal4.png';
 
 function Display2() {
   return (
     <div className='rise__display '>
       <div className='rise__display_buildings'>
           <div className='rise__display_building'>
-            <img src={building1}></img>
+            <div className='rise_display_building_img'>
+            <img src={building89}></img>
+            </div>
               <div className='rise__display_modal1'>
-                <img src={modal1}></img>
+                <img src={modal3}></img>
+              </div>
+              <div className='rise__display_buildinginfo'>
+                <h3>Beanland Building</h3>
+                <h4>Building 1</h4>
+                <button type="button" id='seebtn'>See more</button>
               </div>
           </div>
       </div>
       
       <div className='rise__display_buildings'>
           <div className='rise__display_building'>
-            <img src={building1}></img>
+            <div className='rise_display_building_img'>
+            <img src={building10}></img>
+            </div>
               <div className='rise__display_modal1'>
-                <img src={modal1}></img>
+                <img src={modal4}></img>
+              </div>
+              <div className='rise__display_buildinginfo'>
+                <h3>Beanland Building</h3>
+                <h4>Building 1</h4>
+                <button type="button" id='seebtn'>See more</button>
               </div>
           </div>
       </div>  
