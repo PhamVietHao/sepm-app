eee7e2b48c4506bef4f0554fa7f68b77c8659f3a
PhamVietHao <89537529+PhamVietHao@users.noreply.github.com>
2023-04-10 10:19:39 +0700

    update

--- src/container/display/Display.jsx
+++ src/container/display/Display.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import './display.css'
+import building1 from '../../components/images/building1.png';
+import modal1 from '../../components/images/modal1.png';
 
 function Display() {
   return (
     <div className='rise__display section__padding'>
-      Display
-      dsfsdf
-      dfsdfsd
-      sdfdsf
-      sdfdsfsdfsdf
-      
+      <div className='rise__display_buildings'>
+          <div className='rise__display_building'>
+            <img src={building1}></img>
+              <div className='rise__display_modal1'>
+                <img src={modal1}></img>
+              </div>
+          </div>
+      </div>
+      <div className='rise__display_buildings'>
+          <div className='rise__display_building'>
+            <img src={building1}></img>
+              <div className='rise__display_modal1'>
+                <img src={modal1}></img>
+              </div>
+          </div>
+      </div>  
     </div>
   )
 }
