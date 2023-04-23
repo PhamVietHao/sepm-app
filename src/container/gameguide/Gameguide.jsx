309d01ca212b5079833ff2aa5b5ac7933f6f4eed
PhamVietHao <89537529+PhamVietHao@users.noreply.github.com>
2023-04-23 20:21:55 +0700

    update

--- src/container/gameguide/Gameguide.jsx
+++ src/container/gameguide/Gameguide.jsx
@@ -3,9 +3,12 @@ import './gameguide.css'
 
 function Gameguide() {
   return (
-    <div className='rise__gameguide section__padding'>
+    <div className='rise__gameguide section__padding '>
       <div className='rise__gameguide_header'>
         GAME GUIDE
+      </div>
+      <div className='rise__gameguide_video'>
+      <iframe width="800" height="600" src="https://www.youtube.com/embed/mPZkdNFkNps" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     </div>
   )
