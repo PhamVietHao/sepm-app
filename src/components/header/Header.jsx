971636ac19fdd8f3db4d8ba5c8d2a9cc1587da10
PhamVietHao <89537529+PhamVietHao@users.noreply.github.com>
2023-04-23 14:00:09 +0700

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import {React} from 'react';
+import { TypeAnimation } from 'react-type-animation';
 import './header.css';
 import shield from '../../components/images/shieldlogo.png';
-
 function Header() {
   return (
+    
     <div className='rise__header section__padding' id="home">
-        <div className='rise__header-content'>
+        <div className='rise__header-content' >
             <h1> <div className='bold_RMIT'>RMIT</div> COMPANION</h1>
-            <p>This web-based application is created to help students and parents explore the school infrastructure in a more interactive and information way with a FUN and ENGAGING Website with game and Chatbot features.</p>
+            <p>This web-based application is created to help students and parents explore the school infrastructure in a more interactive and information way with a FUN and ENGAGING Website with&nbsp; 
+            <TypeAnimation
+        sequence={[
+          'Game', 
+          1000, 
+          'Game and', 
+          1050, 
+          'Game and Chatbot', 
+          1100,
+          'Game and Chatbot feartures.',
+          1150,
+          '',
+          () => {
+            console.log('Sequence completed'); 
+          }
+        ]}
+        wrapper="span"
+        cursor={true}
+        repeat={Infinity}
+        // style={{ fontSize: '2em', display: 'inline-block' }}
+      />
+            </p>
             <button type="button">Get Started</button>
         </div>
         <div className='rise__header-image'>
@@ -16,5 +38,7 @@ function Header() {
     </div>
   )
 }
+
+
 
 export default Header
