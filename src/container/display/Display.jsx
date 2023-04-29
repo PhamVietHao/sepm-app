8493c59db37b0b7c7b3c7a7398ccbbce4f5303a6
PhamVietHao <89537529+PhamVietHao@users.noreply.github.com>
2023-04-29 21:31:34 +0700

    update modal

--- src/container/display/Display.jsx
+++ src/container/display/Display.jsx
@@ -11,6 +11,10 @@ function Display() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);  
   const handleShow = () => setShow(true);
+
+  const [show2, setShow2] = useState(false);
+  const handleClose2 = () => setShow2(false);  
+  const handleShow2 = () => setShow2(true);
   
   return (
     <div className='rise__display'>
@@ -27,18 +31,13 @@ function Display() {
                 <h4>Building 1</h4>
                 <button type="button" id='seebtn' onClick={handleShow}>See more</button>
                 <Modal className='modalBackground' show={show} onHide={handleClose}>
-                  <div className='modalContainer'>
+                  <div className='modalContainer section__padding'>
                   <Modal.Header closeButton>
-                    <Modal.Title className='title'>Modal heading</Modal.Title>
+                    <Modal.Title className='title'>ACADEMIC BUILDING 1</Modal.Title>
                   </Modal.Header>
                   <Modal.Body className='body'>Woohoo, you're reading this text in a modal!</Modal.Body>
-                  <Modal.Footer className='footer'>
-                    <Button variant="secondary" onClick={handleClose}>
-                      Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                      Save Changes
-                    </Button>
+                  <Modal.Footer className='modal_footer'>  
+                    <button id='modal_btn' onClick={handleClose}> Close </button>
                   </Modal.Footer>
                   </div>
                 </Modal>
@@ -57,18 +56,18 @@ function Display() {
               <div className='rise__display_buildinginfo building1st'>
                 <h3>Academic Building 2</h3>
                 <h4>Building 2</h4>
-                <button type="button" id='seebtn' onClick={handleShow}>See more</button> 
-                <Modal className='modalBackground' show={show} onHide={handleClose}>
-                  <div className='modalContainer'>
+                <button type="button" id='seebtn' onClick={handleShow2}>See more</button> 
+                <Modal className='modalBackground' show={show2} onHide={handleClose2}>
+                  <div className='modalContainer section__padding'>
                   <Modal.Header closeButton>
-                    <Modal.Title className='title'>Modal heading</Modal.Title>
+                    <Modal.Title className='title'>ACADEMIC BUILDING 2</Modal.Title>
                   </Modal.Header>
                   <Modal.Body className='body'>Woohoo, you're reading this text in a modal!</Modal.Body>
-                  <Modal.Footer className='footer'>
-                    <Button variant="secondary" onClick={handleClose}>
+                  <Modal.Footer className='modal_footer'>
+                    <Button variant="secondary" onClick={handleClose2}>
                       Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={handleClose2}>
                       Save Changes
                     </Button>
                   </Modal.Footer>
--- src/container/display2/Display2.jsx
+++ src/container/display2/Display2.jsx
@@ -12,6 +12,10 @@ function Display2() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);  
   const handleShow = () => setShow(true);
+
+  const [show2, setShow2] = useState(false);
+  const handleClose2 = () => setShow2(false);  
+  const handleShow2 = () => setShow2(true);
   
   return (
     <div className='rise__display '>
@@ -33,7 +37,7 @@ function Display2() {
                     <Modal.Title className='title'>Modal heading</Modal.Title>
                   </Modal.Header>
                   <Modal.Body className='body'>Woohoo, you're reading this text in a modal!</Modal.Body>
-                  <Modal.Footer className='footer'>
+                  <Modal.Footer className='modal_footer'>
                     <Button variant="secondary" onClick={handleClose}>
                       Close
                     </Button>
@@ -58,18 +62,18 @@ function Display2() {
               <div className='rise__display_buildinginfo building2nd'>
                 <h3>Recreation and Events Complex</h3>
                 <h4>Building 10</h4>
-                <button type="button" id='seebtn' onClick={handleShow}>See more</button>
-                <Modal className='modalBackground' show={show} onHide={handleClose}>
+                <button type="button" id='seebtn' onClick={handleShow2}>See more</button>
+                <Modal className='modalBackground' show={show2} onHide={handleClose2}>
                   <div className='modalContainer'>
                   <Modal.Header closeButton>
                     <Modal.Title className='title'>Modal heading</Modal.Title>
                   </Modal.Header>
                   <Modal.Body className='body'>Woohoo, you're reading this text in a modal!</Modal.Body>
-                  <Modal.Footer className='footer'>
-                    <Button variant="secondary" onClick={handleClose}>
+                  <Modal.Footer className='modal_footer'>
+                    <Button variant="secondary" onClick={handleClose2}>
                       Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={handleClose2}>
                       Save Changes
                     </Button>
                   </Modal.Footer>
