2bd613c5b346f631af2d09011acb75ff778d957d
PhamVietHao <89537529+PhamVietHao@users.noreply.github.com>
2023-05-05 21:40:15 +0700

    add animation

--- src/container/rmitbuildings/Rmitbuildings.jsx
+++ src/container/rmitbuildings/Rmitbuildings.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import {React,useEffect} from 'react'
 import RMITlogo from '../../components/images/RMITlogo.png';
 import './rmitbuildings.css'
+import Aos from 'aos';
+
 function Rmitbuildings() {
+  useEffect(()=>{
+      Aos.init({duration:2000})
+  },[])
   return (  
       <div id='ABOUTCAMPUS' className='rise__rmitbuildings section__padding'>
         <div className='rise__rmitbuildings-content'>
@@ -10,7 +15,7 @@ function Rmitbuildings() {
             <p>The Saigon South campus of RMIT university consists of 5 main buildings: Beanland Building, Academic Building 2, Building 8, Residential Building - Building 9 and Recreation and Events Complex - Building 10. In addition, it also has parking lots and a huge sports field. In detail, Beanland Building and Academic Building 2 have 5 floors, Building 8 and Building 9 have 6 floors and Building 10 has 2 levels. In each level of each building, there are many rooms used for various purposes such as classrooms, laboratories, library, office, etc. </p>                  
              
         </div>
-        <div className='rise__rmitbuildings-image'>
+        <div className='rise__rmitbuildings-image' data-aos="fade-left">
             <img src = {RMITlogo} />
         </div>
       </div>
